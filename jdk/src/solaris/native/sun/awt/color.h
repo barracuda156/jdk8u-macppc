61adbc054ac15d91581c1a3ec6cc28fb9626c82f
barracuda156 <vital.had@gmail.com>
2024-05-08 14:22:40 +0800

    Restore X11 for macOS

--- jdk/src/solaris/native/sun/awt/color.h
+++ jdk/src/solaris/native/sun/awt/color.h
@@ -28,13 +28,13 @@
 #include "awt.h"
 #include "colordata.h"
 
-#if !defined(HEADLESS) && !defined(MACOSX)
+#ifndef HEADLESS
 typedef struct {
     unsigned int Depth;
     XPixmapFormatValues wsImageFormat;
     ImgColorData clrdata;
     ImgConvertFcn *convert[NUM_IMGCV];
 } awtImageData;
-#endif /* !HEADLESS && !MACOSX */
+#endif /* !HEADLESS */
 
 #endif           /* _COLOR_H_ */
